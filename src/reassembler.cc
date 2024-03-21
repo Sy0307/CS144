4429887fc8b36968efd68a322c42a9c74c2909da
Sy03 <1370724210@qq.com>
2024-03-21 20:45:06 +0800

    lab0&lab1 finished

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -13,17 +13,17 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     // 如果流已经关闭，或者缓冲区已满，或者当前数据的序号大于期待的序号加上缓冲区的大小
     // 则直接丢弃当前数据
     return;
-  } else if ( first_index + data.size() > expecting_index_ ) {
+  } else if ( first_index + data.size() >= unacceptable_index ) {
     // 如果当前数据的序号加上数据的长度大于期待的序号
     // 则将当前数据截断后存入缓冲区 , 并且不能视作最后的数据
     is_last_substring = false;
     data.resize( unacceptable_index - first_index );
   }
-  if ( first_index > expecting_index_ ) {
+  if ( first_index > expecting_index_ )
     cache_bytes( first_index, move( data ), is_last_substring );
-  } else {
+  else
     push_bytes( first_index, move( data ), is_last_substring );
-  }
+
   flush_buffer();
 }
 
@@ -36,9 +36,9 @@ void Reassembler::push_bytes( uint64_t first_index, string data, bool is_last_su
 {
   if ( first_index < expecting_index_ ) {
     data.erase( 0, expecting_index_ - first_index ); // 删除前面的无效数据
-    expecting_index_ += data.size();
-    output_.writer().push( move( data ) );
   }
+  expecting_index_ += data.size();
+  output_.writer().push( move( data ) );
 
   if ( is_last_substring ) {
     output_.writer().close(); // 关闭流
@@ -50,18 +50,53 @@ void Reassembler::push_bytes( uint64_t first_index, string data, bool is_last_su
 void Reassembler::cache_bytes( uint64_t first_index, string data, bool is_last_substring )
 {
   auto end = unordered_bytes_.end();
-  auto left = lower_bound( unordered_bytes_.begin(), end, first_index, []( const auto&& e, uint64_t idx ) -> bool {
+  auto left = lower_bound( unordered_bytes_.begin(), end, first_index, []( auto&& e, uint64_t idx ) -> bool {
     return idx > ( get<0>( e ) + get<1>( e ).size() );
   } );
 
-  auto right = upper_bound(
-    left, end, first_index + data.size(), []( uint64_t nxt_idx, auto&& e ) { return nxt_idx < get<0>( e ); } );
+  auto right = upper_bound( left, end, first_index + data.size(), []( uint64_t nxt_idx, auto&& e ) -> bool {
+    return nxt_idx < get<0>( e );
+  } );
 
-  const uint64_t next_idx = first_index + data.size();
+  // right 指向是待合并区间的下一个元素
+
+  // 分情况讨论 找到应该插入的位置（包括处理字符串）
+  uint64_t next_idx = first_index + data.size();
   if ( left != end ) {
     auto& [l_point, data_, _] = *left;
-    if ( const uint64_t r_point = l_point + data_.size(); r_point >= first_index )
+    if ( const uint64_t r_point = l_point + data_.size(); r_point >= next_idx && first_index >= l_point ) {
+      // data 本身已经存在了
+      return;
+    } else if ( next_idx < l_point ) {
+      right = left;                                                    // data 和 data_ 之间没有交集
+    } else if ( !( first_index <= l_point && next_idx >= r_point ) ) { // 不是独立的片段
+      if ( first_index >= l_point ) {
+        // data 和 data_ 有交集 , 且 data_ 是在已经缓存的数据的后面
+        data.insert( 0, string_view( data_.c_str(), data_.size() - ( r_point - first_index ) ) );
+      } else {
+        data.resize( data.size() - ( next_idx - l_point ) );
+        data.append( data_ );
+      }
+      first_index = min( first_index, l_point );
+    }
   }
+
+  next_idx = first_index + data.size();
+  if ( right != left && unordered_bytes_.size() ) {
+    auto& [l_point, data_, _] = *prev( right );
+    const uint64_t r_point = l_point + data_.size();
+    if ( r_point > next_idx ) {
+      data.resize( data.size() - ( next_idx - l_point ) );
+      data.append( data_ );
+    }
+  }
+
+  for ( ; left != right; left = unordered_bytes_.erase( left ) ) {
+    num_bytes_pending_ -= get<1>( *left ).size();
+    is_last_substring |= get<2>( *left );
+  }
+  num_bytes_pending_ += data.size();
+  unordered_bytes_.insert( left, make_tuple( first_index, move( data ), is_last_substring ) );
 }
 
 void Reassembler::flush_buffer()
