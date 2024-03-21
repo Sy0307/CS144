bd8aa772aac4d423fb405539f3ab61d76b70a2fa
Sy03 <1370724210@qq.com>
2024-03-21 22:35:38 +0800

    finish wrap&&unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,15 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  constexpr uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
+  const uint32_t checkpoint_mod = wrap( checkpoint, zero_point ).raw_value_;
+  uint32_t distance = raw_value_ - checkpoint_mod;
+  if ( distance <= ( upper >> 1 ) || checkpoint + distance < upper )
+    return checkpoint + distance;
+  return checkpoint + distance - upper;
 }
